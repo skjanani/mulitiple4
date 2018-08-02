33d90e26b4fba81041d6c7756d5ef8b8645d7910
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-02 11:49:24 +0530

    Add files via upload

---
+++ Mulitiple7.java
@@ -0,0 +1,19 @@
+package janani;
+import java.util.Scanner;
+public class Mulitiple7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n;
+System.out.println("enter the number");
+Scanner ja=new Scanner(System.in);
+n=ja.nextInt();
+if(n%7==0){
+	System.out.println("yes");
+}
+else{
+	System.out.println("no");
+}
+	}
+
+}
